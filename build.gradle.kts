85bf1da9a40993babae3a812bc8a6604ba6873fa
offbyone <offbyone@disroot.org>
2022-02-18 22:02:22 +0100

    Fix version string replacement

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ tasks {
         replaceToken("&Name", displayName)
         replaceToken("&name", displayName.toLowerCase())
         replaceToken("&version", version)
-        replaceToken("&minestomVersion", libs.versions.minestom)
+        replaceToken("&minestomVersion", libs.versions.minestom.get())
     }
 
     processResources {
